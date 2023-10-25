1859ac35417b055a4c380b7af53d4b69f7fd84a7
gd-zth <2278336205@qq.com>
2023-10-25 14:53:24 +0800

    v0.0.10

--- src/model/model.go
+++ src/model/model.go
@@ -41,7 +41,7 @@ type PageType struct {
 	Desc bool `form:"desc" bson:"desc" json:"desc"`
 }
 
-// 测试222
+// 测试222 - 1
 func FetchList(mongo *plugin.Mongo, database string, filter plugin.BSON, tables any) (err error) {
 
 	return mongo.FindAll(database, filter, tables)
