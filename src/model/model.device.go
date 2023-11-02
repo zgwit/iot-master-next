0576b51200e4175a7446596d7a3173afcb354c33
gd-zth <2278336205@qq.com>
2023-11-02 14:00:54 +0800

    v0.0.26

--- src/model/model.device.go
+++ src/model/model.device.go
@@ -1,6 +1,8 @@
 package model
 
 type DeviceType struct {
+	CreateTime string `form:"create_time" bson:"create_time" json:"create_time"`
+
 	Name    string `form:"name" bson:"name" json:"name"`
 	Id      string `form:"id" bson:"id" json:"id"`
 	ModelId string `form:"model_id" bson:"model_id" json:"model_id"`
